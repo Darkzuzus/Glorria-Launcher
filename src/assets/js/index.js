9e1a0090fbbe6895b254ef8ff311f1fa84ad1419
Darkzuzus <142235787+Darkzuzus@users.noreply.github.com>
2023-12-10 19:13:51 +0100

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -24,7 +24,7 @@ class Splash {
         let splashes = [
             { "message": "rejoin nous ...", "author": "Darkzuzu" },
             { "message": "Crée en 2023.", "author": "Darkzuzu" },
-            { "message": "🏰 Glorria V.3 🏰", "author": "Darkzuzu" }
+            { "message": "🏰 Glorria V.1.1 🏰", "author": "Darkzuzu" }
         ]
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
